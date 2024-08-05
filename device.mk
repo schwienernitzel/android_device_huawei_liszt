05ff52c177657a22c8357fb77b57214fbd184958
schwienernitzel <pfelix0803@gmail.com>
2024-08-05 22:31:30 +0000

    liszt: Import initial overlay configs
    Signed-off-by: schwienernitzel <pfelix0803@gmail.com>

--- device.mk
+++ device.mk
@@ -41,6 +41,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml
 
+# Overlay
+DEVICE_PACKAGE_OVERLAYS += \
+    $(DEVICE_PATH)/overlay
+
 # Properties
 ADDITIONAL_DEFAULT_PROPERTIES += \
     ro.zygote=zygote64_32 \
