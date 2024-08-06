2eab5f479c37afd8877e38a768304c934b164705
schwienernitzel <pfelix0803@gmail.com>
2024-08-06 09:42:09 +0000

    liszt: rootdir: Fix typo
    Signed-off-by: schwienernitzel <pfelix0803@gmail.com>

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -67,7 +67,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE       := init.hi3635.connectivity.rc
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.connectivity.bcm4334.rc
+LOCAL_SRC_FILES    := etc/init.hi3635.connectivity.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
